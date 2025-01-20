1dcbf82343185b7e8f52147cdd09ed937e1e3c24
nivedita <niveditashastri98@gmail.com>
2025-01-21 01:57:28 +0530

    Initial commit for Netlify deployment

---
+++ next.config.js
@@ -0,0 +1,10 @@
+
+module.exports = {
+    // This tells Next.js to generate static HTML files
+    output: 'export',
+    trailingSlash: true,
+    images: {
+      unoptimized: true,
+    },
+  };
+  
